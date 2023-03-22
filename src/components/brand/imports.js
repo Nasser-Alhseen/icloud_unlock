1495cd8cfec978612dfb3c7e2bd433172d1057d4
Nasser Alhseen <abdalnasralhseen2019@gmail.com>
2023-03-22 19:23:02 +0200

    inital

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/ipad.png';
+import slack from '../../assets/iphone.png';
+import atlassian from '../../assets/smartwatch-app.png';
+import dropbox from '../../assets/macbook.png';
+import shopify from '../../assets/shopify.png';
+
+export {
+  google,
+  slack,
+  atlassian,
+  dropbox,
+  shopify,
+};
